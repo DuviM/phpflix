08f1c5b1847cef973caacc4a28fb53808c43f9b2
Mathieu Duvillers <duvillers@gmail.com>
2023-09-19 16:17:42 +0200

    db

---
+++ aboutPage.php
@@ -0,0 +1,10 @@
+<h1 class="page-title">PHPLIMZ</h1>
+<div class="dev-card">
+  <img src="./img/Mathieu.jpg" alt="">
+  <p>Mathieu Duvillers</p>
+  <p>Dev Web en reconversion</p>
+  <div class="logo-social-networks">
+
+  </div>
+  <p>Recrutez moi</p>
+</div>
--- moviePage.php
+++ moviePage.php
@@ -1,33 +1,33 @@
 <?php
- $find = false;
- $data = array("title" => "Pas de film a cette adresse");
- if (isset($_GET["id"])){
-   $json = file_get_contents("movies.json");
-   $movies = json_decode($json, true);
+$find = false;
+$data = array("title" => "Pas de film a cette adresse");
+if (isset($_GET["id"])) {
+  $json = file_get_contents("movies.json");
+  $movies = json_decode($json, true);
 
-   foreach ($movies as $movie) {
-     if($movie["id"] == $_GET["id"]) {
-       $find = true;
-       $data = $movie;
-     }
-   }
- }
- 
- $json = file_get_contents("actors.json");
- $actors = json_decode($json, true);
- 
- $json = file_get_contents("genres.json");
- $genres = json_decode($json, true);
+  foreach ($movies as $movie) {
+    if ($movie["id"] == $_GET["id"]) {
+      $find = true;
+      $data = $movie;
+    }
+  }
+}
 
- include("template/header.php");
+$json = file_get_contents("actors.json");
+$actors = json_decode($json, true);
+
+$json = file_get_contents("genres.json");
+$genres = json_decode($json, true);
+
+include("template/header.php");
 ?>
 <?php
 if ($find) {
 ?>
-<div class="single-movie-container">
-  <div class="single-movie-background">
-  <img src="<?= $data["backdrop_path"] ?>" alt="" class="single-movie-backdrop-image">
-  <div class="single-movie-depiction">
+  <div class="single-movie-container">
+    <div class="single-movie-background">
+      <img src="<?= $data["backdrop_path"] ?>" alt="" class="single-movie-backdrop-image">
+      <div class="single-movie-depiction">
         <div class="single-movie-poster">
           <img src="<?= $data["poster_path"] ?>" alt="">
         </div>
@@ -35,29 +35,26 @@ if ($find) {
           <div class="single-movie-title">
             <p class="single-movie-original-title">
               <?= $data["original_title"] ?>
-            </p>
-            <p class="single-movie-year">
-              (<?= date ("Y", strtotime ($data["release_date"])) ?>)
+              <span class="single-movie-year">
+              (<?= date("Y", strtotime($data["release_date"])) ?>)
+              </span>
             </p>
           </div>
           <div class="single-movie-date">
             <p class="single-movie-release-date">
-            <?= date("d F Y", strtotime($movie["release_date"])) ?>
+              <?= date("d F Y", strtotime($movie["release_date"])) ?>
             </p>
             <p class="single-movie-category">
               <?= $data["genreId"] ?>
             </p>
             <p class="single-movie-duration">
-              <?= sprintf("%dh%02d",   floor($data["runtime"]/60), $data["runtime"]%60); ?>
+              <?= sprintf("%dh%02d",   floor($data["runtime"] / 60), $data["runtime"] % 60); ?>
             </p>
           </div>
           <div class="single-movie-rating">
             <div class="single-movie-rating-bar">
-              <span class="fa fa-star"></span>
-              <span class="fa fa-star"></span>
-              <span class="fa fa-star"></span>
-              <span class="fa fa-star"></span>
-              <span class="fa fa-star"></span>
+              <img src="./img/ratingStars.png" alt="" class="single-movie-rating-bar-stars">
+              <span class="single-movie-rating-bar-note" style="width:<?= floor($data["vote_average"] * 10); ?>%;"></span>
             </div>
             <p class="single-movie-note">
               <?= floor($data["vote_average"] * 10); ?> %
@@ -75,11 +72,11 @@ if ($find) {
             </p>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="casting">
     </div>
   </div>
-  <div class="casting">
-  </div>
-</div>
 
 <?php
 }
